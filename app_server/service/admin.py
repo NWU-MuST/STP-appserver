1b43472ecb2c58913facbcdc5d0026d763e79667
Daniel van Niekerk <dvn.demitasse@gmail.com>
2016-05-10 18:38:29 +0200

    implementation and basic testing of the remaining Admin functionality...

--- app_server/service/admin.py
+++ app_server/service/admin.py
@@ -13,14 +13,18 @@ import json
 import auth
 from httperrs import BadRequestError, ConflictError, NotFoundError
 
+def hashpassw(password):
+    salt = bcrypt.gensalt()
+    pwhash = bcrypt.hashpw(password, salt)
+    return salt, pwhash
+
 class Admin(auth.UserAuth):
     """Implements all functions related to updating user information in
        the auth database.
     """
     def add_user(self, request):
         auth.token_auth(request["token"], self._config["authdb"])
-        salt = bcrypt.gensalt()
-        pwhash = bcrypt.hashpw(request["password"], salt)
+        salt, pwhash = hashpassw(request["password"])
         try:
             with sqlite.connect(self._config["target_authdb"]) as db_conn:
                 db_curs = db_conn.cursor()
@@ -56,3 +60,33 @@ class Admin(auth.UserAuth):
             else:
                 username, pwhash, salt, name, surname, email = entry
                 return {"name": name, "surname": surname, "email": email}
+
+    def update_user(self, request):
+        auth.token_auth(request["token"], self._config["authdb"])
+        with sqlite.connect(self._config["target_authdb"]) as db_conn:
+            db_curs = db_conn.cursor()
+            #User exists?
+            db_curs.execute("SELECT * FROM users WHERE username=?", (request["username"],))
+            entry = db_curs.fetchone()
+            if entry is None:
+                raise NotFoundError("User not registered")
+            #Proceed to update
+            for field in ["name", "surname", "email"]:
+                if field in request:
+                    db_curs.execute("UPDATE users SET {}=? WHERE username=?".format(field), (request[field], request["username"]))
+            if "password" in request:
+                salt, pwhash = hashpassw(request["password"])
+                db_curs.execute("UPDATE users SET pwhash=? WHERE username=?", (pwhash, request["username"]))
+                db_curs.execute("UPDATE users SET salt=? WHERE username=?", (salt, request["username"]))
+            db_conn.commit()
+        return "User info updated"
+
+    def get_users(self, request):
+        auth.token_auth(request["token"], self._config["authdb"])
+        users = dict()
+        with sqlite.connect(self._config["target_authdb"]) as db_conn:
+            db_curs = db_conn.cursor()
+            for entry in db_curs.execute("SELECT * FROM users"):
+                username, pwhash, salt, name, surname, email = entry
+                users[username] = {"name": name, "surname": surname, "email": email}
+        return users
