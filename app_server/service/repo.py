4f5bbb0a487409fe23e68790edf4988384dd32f2
Daniel van Niekerk <dvn.demitasse@gmail.com>
2016-05-30 15:24:21 +0200

    minor revisions...

--- app_server/service/repo.py
+++ app_server/service/repo.py
@@ -4,7 +4,7 @@
    simplified for our application (i.e. working with single files
    isolated in different directories).
 
-   This DOES NOT ALLOW SAFE CONCURRENT ACCESS to a shared `repodir`
+   This DOES NOT ALLOW SAFE CONCURRENT ACCESS to a shared `workingdir`
 """
 from __future__ import print_function, division, unicode_literals
 
@@ -19,7 +19,7 @@ class RepoError(Exception):
     pass
 
 
-def check(repodir, commit_id=None):
+def check(workingdir, commit_id=None):
     """Make sure that:
           -- Repo path exists
           -- There are no uncommitted changes
@@ -30,7 +30,7 @@ def check(repodir, commit_id=None):
        where relevant.
     """
     try:
-        r = git.Repo(repodir)
+        r = git.Repo(workingdir)
     except NoSuchPathError:
         raise RepoError("Path")
     except InvalidGitRepositoryError:
@@ -46,35 +46,35 @@ def check(repodir, commit_id=None):
     return True
 
 
-def init(repodir):
-    git.Repo.init(repodir)
+def init(workingdir):
+    git.Repo.init(workingdir)
 
 
-def commit(repodir, fbasename, message):
+def commit(workingdir, fbasename, message):
     """Ideally repo.check() has been called already externally.
-       `fbasename` is assumed to be in `repodir`
+       `fbasename` is assumed to be in `workingdir`
     """
-    r = git.Repo(repodir)
-    filename = os.path.join(repodir, fbasename)
+    r = git.Repo(workingdir)
+    filename = os.path.join(workingdir, fbasename)
     r.index.add([filename])
     c = r.index.commit(message)
     return c.hexsha
 
 
-def revert(repodir, fbasename, commit_id, message=None):
+def revert(workingdir, fbasename, commit_id, message=None):
     """Ideally repo.check() has been called already externally.
 
        This checks out a file at previous commit and recommits it,
        i.e. reverts a file without losing subsequent edit history.
     """
-    r = git.Repo(repodir)
+    r = git.Repo(workingdir)
     r.git.checkout(commit_id, fbasename)
     if message is None:
         message = "reverted '{}' to {}".format(fbasename, commit_id)
-    return commit(repodir, fbasename, message)
+    return commit(workingdir, fbasename, message)
 
 
-def rollback(repodir, commit_id=None):
+def rollback(workingdir, commit_id=None):
     """This is a HARD ROLLBACK to a previous commit, i.e. subsequent
        history is lost. This should not be used by users to undo
        recent saves (use `revert` instead), but may be used by scripts
@@ -82,7 +82,7 @@ def rollback(repodir, commit_id=None):
 
        The previous commit (HEAD) is used if `commit_id` is None
     """
-    r = git.Repo(repodir)
+    r = git.Repo(workingdir)
     r.git.reset("--hard", commit_id)
 
 
@@ -90,8 +90,7 @@ def rollback(repodir, commit_id=None):
 def test():
     """Informal tests...
     """
-    import os
-    import tempfile
+    import os, shutil, tempfile
     import repo
     ### 1
     try:
@@ -107,53 +106,54 @@ def test():
         assert str(e) == "Format"
         print("TEST_2 SUCCESS:", "Not a repo caught!")
     #CREATE REPO
-    repodir = os.path.join(tempd, "test")
-    repo.init(repodir)
+    workingdir = os.path.join(tempd, "test")
+    repo.init(workingdir)
     ### 3
     try:
-        repo.check(repodir)
+        repo.check(workingdir)
     except repo.RepoError as e:
         assert str(e) == "Empty"
         print("TEST_3 SUCCESS:", "Empty repo caught!")
     #CREATE FILE AND COMMIT
     fbasename = "a"
-    filename = os.path.join(repodir, fbasename)
+    filename = os.path.join(workingdir, fbasename)
     with open(filename, "w") as wfh:
         wfh.write("first edits\n")
-    cid1 = repo.commit(repodir, fbasename, "commit 1")
-    print("Commited file to repo, commit id:", cid1)
+    cid1 = repo.commit(workingdir, fbasename, "commit 1")
+    print("\tCommited file to repo, commit id:", cid1)
     ### 4
     try:
-        repo.check(repodir, cid1 + "not_correct_id")
+        repo.check(workingdir, cid1 + "not_correct_id")
     except repo.RepoError as e:
         assert str(e) == "Inconsistent"
         print("TEST_4 SUCCESS:", "Wrong commit ID caught!")
     #CREATE PARTIAL COMMIT
-    repo.check(repodir, cid1)
+    repo.check(workingdir, cid1)
     with open(filename, "w") as wfh:
         wfh.write("unsuccessful edits\n")
-    r = git.Repo(repodir)
+    r = git.Repo(workingdir)
     r.index.add([filename])
     ### 5
     try:
-        repo.check(repodir, cid1)
+        repo.check(workingdir, cid1)
     except repo.RepoError as e:
         assert str(e) == "Uncommitted"
         print("TEST_5 SUCCESS:", "Partial commit process caught!")
     #
-    repo.rollback(repodir)
-    repo.check(repodir, cid1)
+    repo.rollback(workingdir)
+    repo.check(workingdir, cid1)
     with open(filename, "w") as wfh:
         wfh.write("successful edits\n")
-    cid2 = repo.commit(repodir, fbasename, "commit 2")
-    print("Commited file update to repo, commit id:", cid2)
+    cid2 = repo.commit(workingdir, fbasename, "commit 2")
+    print("\tCommited file update to repo, commit id:", cid2)
     ### 6
-    repo.check(repodir, cid2)    
-    cid3 = repo.revert(repodir, fbasename, cid1)
-    print("Reverting file to first commit, new commit id:", cid3)
-    if repo.check(repodir, cid3) and open(filename).read() == "first edits\n":
+    repo.check(workingdir, cid2)    
+    cid3 = repo.revert(workingdir, fbasename, cid1)
+    print("\tReverting file to first commit, new commit id:", cid3)
+    if repo.check(workingdir, cid3) and open(filename).read() == "first edits\n":
         print("TEST_6 SUCCESSFUL:", "Successfully reverted file to previous version")
-    
+    #
+    shutil.rmtree(tempd)
 
 
 if __name__ == "__main__":
